0c854c0095811e6acf3a17f152891f332be236c5
Sneha <bhardwajsneha111@gmail.com>
2024-04-18 12:24:16 +0530

    react

---
+++ app.js
@@ -0,0 +1,4 @@
+const heading= React.createElement("h1",{id: "myh1"},"hello how are you doing");
+const root= ReactDOM.createRoot(document.getElementById("root"));
+const r= React.ce
+root.render(heading);
